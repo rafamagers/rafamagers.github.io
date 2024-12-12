11efc62ba9c7c0a47fdfd768534df74fb83414b0
Rafael <ramontero@uninorte.edu.co>
2024-12-12 00:28:37 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rafamagers.github.io/', // Cambia 'nombre-del-repositorio' por el nombre real de tu repo
+
 })
